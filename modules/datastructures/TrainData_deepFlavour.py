f912e636ad568ec773a179eea42ee47a29c50955
jkiesele <jkiesele@cern.ch>
2018-01-31 11:39:22 +0100

    first restructuring - not working

--- modules/datastructures/TrainData_deepFlavour.py
+++ modules/datastructures/TrainData_deepFlavour.py
@@ -12,8 +12,8 @@ from keras.layers.pooling import MaxPooling2D
 #from tensorflow.python.ops import control_flow_ops 
 #tensorflow.python.control_flow_ops = control_flow_ops
 
-from TrainData import TrainData_fullTruth
-from TrainData import TrainData,fileTimeOut,TrainData_QGOnly
+from TrainDataDeepJet import TrainData_fullTruth
+from TrainDataDeepJet import fileTimeOut,TrainData_QGOnly
 
 class TrainData_deepFlavour_FT(TrainData_fullTruth):
     '''
